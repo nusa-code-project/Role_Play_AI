8fa696b37b76cbea883b4c53f5e22491a7bc126b
Daffdhot29 <daff.yunus05@gmail.com>
2025-10-13 22:08:38 +0700

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
-api_key = "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"
-client = OpenAI(api_key=api_key)
+# api_key = "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"
+# client = OpenAI(api_key=api_key)
 
 response = client.responses.create(
     model="gpt-5",
